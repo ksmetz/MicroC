b7bbd71199dd10e42aee41c71fd3f8b5f1a0ddfb
Kathleen Reed <reedks@biowulf.nih.gov>
2023-02-06 12:20:45 -0500

    Added merge workflow. Tested and confirmed functional, though lacking multithreading and tempdir params (which appear to break it)

--- unlock.sh
+++ unlock.sh
@@ -11,6 +11,7 @@ source env/bin/activate
 
 ## Unlock snakemake workflow
 snakemake -j 100 --unlock -s workflows/alignFASTQ --max-jobs-per-second 5 --max-status-checks-per-second 5 --rerun-incomplete --restart-times 3 -p --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch -J {cluster.name} -p {cluster.partition} -t {cluster.time} -c {cluster.cpusPerTask} --mem-per-cpu={cluster.memPerCpu} -N {cluster.nodes} --output {cluster.output} --error {cluster.error} --parsable" --cluster-status ./scripts/status.py
+snakemake -j 100 --unlock -s workflows/mergeHIC --max-jobs-per-second 5 --max-status-checks-per-second 5 --rerun-incomplete --restart-times 3 -p --latency-wait 500 --cluster-config "config/cluster.yaml" --cluster "sbatch -J {cluster.name} -p {cluster.partition} -t {cluster.time} -c {cluster.cpusPerTask} --mem-per-cpu={cluster.memPerCpu} -N {cluster.nodes} --output {cluster.output} --error {cluster.error} --parsable" --cluster-status ./scripts/status.py
 
 ## Deactivate virtual environment
 deactivate
